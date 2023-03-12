cec6608d8faa0cc42552f6a3334f1d074250f0a7
ziguayungui <244753440@qq.com>
2023-03-12 23:11:24 +0800

    optimize code

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 ROOT_PATH=${PWD}
 BUILD_DIR=${ROOT_PATH}/openwrt
 TARGET=${1}
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
 
 if [ -z "$1" ]; then
 	echo "Error: please specify TARGET"
@@ -23,8 +25,19 @@ sed -i "s= [a-z]*:.*/feed= https://gitee.com/wlan-ap=g" openwrt/feeds.conf.defau
 
 [ ! -f ${BUILD_DIR}/scripts/gen_config.py ] && cp ${ROOT_PATH}/scripts/gen_config.py ${BUILD_DIR}/scripts/gen_config.py
 cp ${ROOT_PATH}/scripts/feeds ${BUILD_DIR}/scripts/feeds
+
 cd ${BUILD_DIR}
-./scripts/gen_config.py ${TARGET} || exit 1
+# release version
+date '+%Y%m%d%H' > version
+
+# rm file
+# latest_version=21.02.5
+# [ -f Packages.gz ] && rm Packages.gz
+# wget https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/${latest_version}/targets/x86/64/packages/Packages.gz
+# zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > kernel.vermagic
+
+# generate build config 
+${BUILD_DIR}/scripts/gen_config.py ${TARGET} || exit 1
 cd -
 
 echo "### Building image ..."
